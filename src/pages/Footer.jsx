15f68b27a7f85c9634ef1f86f64432c19b9e1159
rafatparween <rafatparween27392@gmail.com>
2025-03-18 02:51:06 +0530

    s remove from service

---
+++ src/pages/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-full bg-[#3730a3] text-white text-center py-4">
+      <p>Copyright © 2025 ijprinterlivesolution All Rights Reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
